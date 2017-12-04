b0085f156313a101172f1085b44378b870ae689a
xiangsanliu <xiangjianjian@hotmail.com>
2017-12-04 20:46:58 +0800

    还差一点，而且改了界面

--- app/src/main/java/org/chengjian/java/feidian/collectdata/mvp/ui/activities/base/DetailBaseActivity.java
+++ app/src/main/java/org/chengjian/java/feidian/collectdata/mvp/ui/activities/base/DetailBaseActivity.java
@@ -2,11 +2,14 @@ package org.chengjian.java.feidian.collectdata.mvp.ui.activities.base;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -122,5 +125,20 @@ public abstract class DetailBaseActivity extends BaseActivity {
     }
 
 
+    public int getScreenWidth() {
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+
+        return wm.getDefaultDisplay().getWidth();
+
+    }
+
+    public int pixelToDp(int pixel) {
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;// 屏幕宽度（像素）
+        float density = dm.density;//屏幕密度（0.75 / 1.0 / 1.5）
+        return (int) (width/density);
+    }
 
 }
---
+++ app/src/main/java/org/chengjian/java/feidian/collectdata/mvp/ui/activities/dataAdder/CommercialHouseTradeActivity.java
@@ -0,0 +1,88 @@
+package org.chengjian.java.feidian.collectdata.mvp.ui.activities.dataAdder;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import net.cachapa.expandablelayout.ExpandableLayout;
+
+import org.chengjian.java.feidian.collectdata.R;
+import org.chengjian.java.feidian.collectdata.beans.SellRentModel;
+import org.chengjian.java.feidian.collectdata.databinding.ActivityCommercialHouseTradeBinding;
+import org.chengjian.java.feidian.collectdata.databinding.ChildBaseBinding;
+import org.chengjian.java.feidian.collectdata.mvp.ui.activities.base.DetailBaseActivity;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+public class CommercialHouseTradeActivity extends DetailBaseActivity implements View.OnClickListener {
+
+    ActivityCommercialHouseTradeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_commercial_house_trade);
+        initExpandableLayout();
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initExpandableLayout() {
+        binding.childBase.buttonExtra.setOnClickListener(this);
+        binding.childChsBuildingSituation.buttonBuildingSituation.setOnClickListener(this);
+        binding.childChsLandSituation.buttonLandSituation.setOnClickListener(this);
+    }
+
+    @Override
+    public void initSpinner() {
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
+    public void onEventMainThread(SellRentModel sellRentModel) {
+        this.sellRentModel = sellRentModel;
+    }
+
+    @Override
+    public void getSpinner() {
+
+    }
+
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_extra:
+                changeELState(binding.childBase.elExtra);
+                binding.childChsBuildingSituation.elBuildingSituation.collapse();
+                binding.childChsLandSituation.elLandSituation.collapse();
+                break;
+            case R.id.button_building_situation:
+                changeELState(binding.childChsBuildingSituation.elBuildingSituation);
+                binding.childBase.elExtra.collapse();
+                binding.childChsLandSituation.elLandSituation.collapse();
+                break;
+            case R.id.button_land_situation:
+                changeELState(binding.childChsLandSituation.elLandSituation);
+                binding.childChsBuildingSituation.elBuildingSituation.collapse();
+                binding.childBase.elExtra.collapse();
+                break;
+        }
+    }
+}
