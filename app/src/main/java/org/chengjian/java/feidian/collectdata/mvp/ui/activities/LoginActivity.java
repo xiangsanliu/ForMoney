b9e74a3578699e91d07184f33985781c5cfd5d26
xiang <xiangjianjian@hotmail.com>
2017-12-27 15:07:15 +0800

    加了一行代码

--- app/src/main/java/org/chengjian/java/feidian/collectdata/mvp/ui/activities/LoginActivity.java
+++ app/src/main/java/org/chengjian/java/feidian/collectdata/mvp/ui/activities/LoginActivity.java
@@ -50,6 +50,7 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
         initVariables();
         initViews();
     }
